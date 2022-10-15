93d94dbbfa244b353156e617e0049dcf04d5befc
razmy-mahsoom <razmy_mahsoom@hotmail.com>
2022-10-15 20:36:23 +0530

    ini

--- src/main/java/org/microq/demo/config/MQConfig.java
+++ src/main/java/org/microq/demo/config/MQConfig.java
@@ -3,10 +3,8 @@ package org.microq.demo.config;
 import org.microq.support.auditor.Chaining;
 import org.microq.support.auditor.Interchange;
 import org.microq.support.auditor.Sequence;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 public class MQConfig {
@@ -30,20 +28,6 @@ public class MQConfig {
                 .build();
     }
 
-    @Bean
-    public Interchange testInterchange(){
-        return new Interchange("default");
-    }
-    @Bean
-    public Sequence testSequence(){
-        return new Sequence("default");
-    }
-    @Bean
-    public Chaining testChaining(){
-        return Chaining.builder()
-                .chain(testSequence())
-                .to(testInterchange())
-                .withPath("test-to-test")
-                .build();
-    }
+
+
 }
